b500ecaabdb8e32c7bef7acf965ddea7a4e9a1c1
Jamshid Ganiev <jamesganiev227@gmail.com>
2023-01-29 14:52:34 +0900

    Remove celery broke url

--- config/settings.py
+++ config/settings.py
@@ -146,8 +146,6 @@ STATIC_ROOT = 'static-files'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-CELERY_BROKER_URL = env("BROKER_URL")
-
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
